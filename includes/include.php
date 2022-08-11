1ddae78aa46ecb320c38bd9dda6fcf6bed134ad1
HSchafferWS <hschaffer@widesoft.net>
2022-08-11 16:36:24 -0300

    includes

---
+++ includes/include.php
@@ -0,0 +1,10 @@
+<div class="titulo">Includes</div>
+
+<?php
+
+echo 'Arquivo Carregado... Include.php <br>';
+
+include('include_arquivo.php');
+
+echo soma(3, 8) . '!<br>';
+echo "O conteudo da variavel é '{$variavel}'.";
