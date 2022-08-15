1337c2b271ebbac9e2577c1f83d5db2e70b982a3
HSchafferWS <hschaffer@widesoft.net>
2022-08-15 14:44:48 -0300

    inicio modulo tratamento de erro

--- menu.php
+++ menu.php
@@ -1,10 +1,20 @@
 <nav class="modulos">
-<div class="modulo laranja-escuro">
+    <div class="modulo laranja-escuro">
         <h3>10. Namespace</h3>
         <ul>
             <li>
                 <a href="exercicio.php?dir=namespace&file=basico">
-                    Basico
+                    Exemplo Básico
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=namespace&file=sub_namespaces">
+                    Sub-Namespaces
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=namespace&file=use_as">
+                    Use/As
                 </a>
             </li>
         </ul>
