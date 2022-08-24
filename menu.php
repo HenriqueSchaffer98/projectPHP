f911f5e9ef368088f5a1cc085cbaea4041b8bedf
HSchafferWS <hschaffer@widesoft.net>
2022-08-24 15:40:20 -0300

    inicio de módulo

--- menu.php
+++ menu.php
@@ -1,15 +1,25 @@
 <nav class="modulos">
-<div class="modulo verde">
-        <h3>11. Tratamento de Erros</h3>
+    <div class="modulo verde">
+        <h3>11. Trat. de Erro</h3>
         <ul>
             <li>
                 <a href="exercicio.php?dir=tratamento_erro&file=try_catch">
-                    Try && Catch
+                    Try/Catch
                 </a>
             </li>
             <li>
                 <a href="exercicio.php?dir=tratamento_erro&file=erros_personalizados">
                     Erros Personalizados
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=tratamento_erro&file=desafio_intdiv_teste">
+                    Desafio intdiv
+                </a>
+            </li>
+            <li>
+                <a href="exercicio.php?dir=tratamento_erro&file=gerenciador_erro">
+                    Error Handler
                 </a>
             </li>
         </ul>
